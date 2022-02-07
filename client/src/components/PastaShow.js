e95d9491dd55afbcd360a87909bef9b97a6a827d
mstacey-codes <90103768+mstacey-codes@users.noreply.github.com>
2022-02-07 14:20:48 -0500

    style for a second time

--- client/src/components/PastaShow.js
+++ client/src/components/PastaShow.js
@@ -1,42 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const PastaShow = (props) => {
-    const [pasta, setPasta] = useState({
-        name: '',
-        description: '',
-        category: {}
-    })
+  const [pasta, setPasta] = useState({
+    name: "",
+    description: "",
+    category: {},
+  });
 
-    const pastaId = props.match.params.id
+  const pastaId = props.match.params.id;
 
-    const getPasta = async () => {
-        
-        try {
-            const response = await fetch(`/api/v1/pastas/${pastaId}`)
-            if (!response.ok) {
-                const errorMessage = `${response.status} (${response.statusText})`
-                const error = new Error(errorMessage)
-                throw error
-            }
-            const body = await response.json()
-            setPasta(body.pasta)
-        } catch (error) {
-            console.error(`Error in fetch ${error.message}`)
-        }
+  const getPasta = async () => {
+    try {
+      const response = await fetch(`/api/v1/pastas/${pastaId}`);
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`;
+        const error = new Error(errorMessage);
+        throw error;
+      }
+      const body = await response.json();
+      setPasta(body.pasta);
+    } catch (error) {
+      console.error(`Error in fetch ${error.message}`);
     }
+  };
 
-    useEffect(() => {
-        getPasta()
-    }, [])
-    return (
-        <>
-            <div className="pasta-info">
-                <h1 className='pasta-title'>{pasta.name}</h1>
-                <p className="pasta-desc">{pasta.description}</p>
-                <p className="pasta-category">Category: {pasta.category.name}</p>
-            </div>
-        </>
-    )
-}
+  useEffect(() => {
+    getPasta();
+  }, []);
+  return (
+    <>
+      <h2 className="pasta-title">{pasta.name}</h2>
+      <div className="pasta-info">
+        <p className="pasta-desc">{pasta.description}</p>
+        <p className="pasta-category">Category: {pasta.category.name}</p>
+      </div>
+    </>
+  );
+};
 
-export default PastaShow
+export default PastaShow;
